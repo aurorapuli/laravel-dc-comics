8c433dff66799e0cc909fa42f6ec5b7a4c598fa1
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-02-15 16:19:07 +0100

    add: struttura tabella

---
+++ database/migrations/2024_02_15_144821_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('nome');
+            $table -> string('genere');
+            $table -> string('autore');
+            $table -> integer('scaffale');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- database/seeders/BookTableSeeder.php
+++ database/seeders/BookTableSeeder.php
@@ -5,18 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Book;
+
+class BookTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this -> call([
-
-            BookTableSeeder :: class
-        ]);
+        Book :: factory() -> count(30) -> create();
     }
 }
